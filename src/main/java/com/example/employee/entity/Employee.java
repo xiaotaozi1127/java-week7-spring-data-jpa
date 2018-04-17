a4aab43933f1fdbba48ee768209fdaacf1a9bfae
htao <htao@thoughtworks.com>
2018-04-17 17:39:45 +0800

    [taohui] create employee entity

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,48 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Employee {
+    @Id
+    private int id;
+    private int companyId;
+    private int salary;
+    private String gender;
+    private int age;
+    private String name;
+
+    public Employee(String name, int age, String gender, int salary, int id, int companyId) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.id = id;
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
